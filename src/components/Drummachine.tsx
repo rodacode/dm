51e681d1006b6ab5098b07700ea018497cc90213
Nico <nicolas.hardmeier@yahoo.es>
2019-09-23 12:37:16 +0200

    added Howler and samples

--- src/components/Drummachine.tsx
+++ src/components/Drummachine.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Sequencer } from 'react-nexusui';
 
-export default class Drummachine extends Component {
+// Set the model of the state we need so Typescript intellisense works
+export interface State {
+    rows: number;
+    columns: number;
+    isPlaying: Boolean;
+
+
+}
+export default class Drummachine extends Component<{}, State> {
     sequencer: any;
     ctx: AudioContext;
-    isPlaying: Boolean = false;
 
     constructor(props: any) {
         super(props);
@@ -12,39 +19,42 @@ export default class Drummachine extends Component {
         this.ctx = new AudioContext;
         this.state = {
             rows: 5,
-            columns: 10
+            columns: 8,
+            isPlaying: false
         };
         // This binding is necessary to make `this` work in the callback
         this.play = this.play.bind(this);
+        this.checkSound = this.checkSound.bind(this);
     }
 
     play() {
-        if (this.isPlaying) {
+        console.log('Playing: ' + this.state.isPlaying)
+        if (this.state.isPlaying === true) {
             this.sequencer.stop()
         } else {
             this.sequencer.start(500)
         }
-        this.isPlaying = !this.isPlaying
+        this.setState({ isPlaying: !this.state.isPlaying })
     }
     checkSound(v) {
             for (let i = 0; i < 5; i++) {
                 if (v[i] === 1) {
                     //this.sound1.play()
-                    console.log('hit!!')
+                    console.log('hit number' + i)
                 }
             }
     }
     render() {
 
         return (
-            <div className="syren__container">
+            <div className="container">
                 <h1>Drum Machine</h1>
                 <button onClick={this.play}>Play</button>
                 <div id="sequencer">
                     <Sequencer
-                        rows={5}
-                        columns={10}
-                        size={[400, 200]}
+                        rows={this.state.rows}
+                        columns={this.state.columns}
+                        size={[300, 100]}
                         onStep={this.checkSound}
                         onReady={sequencer => (this.sequencer = sequencer)}
                     />                </div>
